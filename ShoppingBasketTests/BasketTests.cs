89956fce90c911f5f3396fdfc6614aff0e542aad
Sun Quanbin <sunq@edgehill.ac.uk>
2018-02-21 17:33:57 +0000

    Add an example of Unit Test

---
+++ ShoppingBasketTests/BasketTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingBasket;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasket.Tests
+{
+    [TestClass()]
+    public class BasketTests
+    {
+        [TestMethod()]
+        public void GreetingTest()
+        {
+            using (var sw = new StringWriter())
+            {
+                // Arrange
+                Basket basket = new Basket("na");
+                Console.SetOut(sw);
+
+                // Act
+                basket.Greeting();
+                var output = sw.ToString();
+
+                // Assert
+                Assert.AreEqual("Hello, na" + Environment.NewLine, output);
+            }
+
+        }
+    }
+}
